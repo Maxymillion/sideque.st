429073035d2b62a289f85be5ffeb5260c3d162fc
Max van Essen <max.vessen@icloud.com>
2023-05-24 09:16:17 +0200

    Update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1>Sidequest</h1>
-        <p>Oops, it seems you{"\'"}re too early!</p>
+          <Image
+              src="/sidequest.svg"
+              alt="Sidequest Logo"
+              className={styles.logo}
+              width={130}
+              height={33}
+              priority
+          />
+        <p>It seems you{"\'"}re a little too early. There is nothing here yet, but coming quite soon.</p>
       </div>
     </main>
   )
