83861cdef3ca222bd6505fff83a20b707aa1dbee
Max van Essen <max.vessen@icloud.com>
2023-05-26 13:50:41 +0200

    Add prettier

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,33 @@
-import {NextRequest, NextResponse} from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-    matcher: ["/", "/index"],
-}
+  matcher: ["/", "/index"],
+};
 
 export function middleware(req: NextRequest) {
-    const username: string = process.env.DEV_USERNAME ?? "dev";
-    const password: string = process.env.DEV_PASSWORD ?? "dev";
+  const username: string = process.env.DEV_USERNAME ?? "dev";
+  const password: string = process.env.DEV_PASSWORD ?? "dev";
 
-    // Enable middleware on dev or staging subdomains
-    if (!req.nextUrl.hostname.startsWith("dev.") && !req.nextUrl.hostname.startsWith("staging.")) {
-        return NextResponse.next();
+  // Enable middleware on dev or staging subdomains
+  if (
+    !req.nextUrl.hostname.startsWith("dev.") &&
+    !req.nextUrl.hostname.startsWith("staging.")
+  ) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === username && pwd === password) {
+      return NextResponse.next();
     }
+  }
+  url.pathname = "/api/auth";
 
-    const basicAuth = req.headers.get("authorization");
-    const url = req.nextUrl;
-
-    if (basicAuth) {
-        const authValue = basicAuth.split(" ")[1];
-        const [user, pwd] = atob(authValue).split(":");
-
-        if (user === username && pwd === password) {
-            return NextResponse.next();
-        }
-    }
-    url.pathname = "/api/auth";
-
-    return NextResponse.rewrite(url);
+  return NextResponse.rewrite(url);
 }
