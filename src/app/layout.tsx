804cb11149111be25f3681963a16c47235452a9d
Max van Essen <max.vessen@icloud.com>
2023-05-27 10:03:28 +0200

    Add more favicons and clean up meda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.scss";
 import { Inter } from "next/font/google";
+import Favicons from "@/components/Favicons";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Sidequest - Coming Soon",
   description: "Coming soon!",
+  icons: Favicons,
 };
 
 export default function RootLayout({ children }) {
---
+++ src/components/Favicons/index.tsx
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  icon: [
+    {
+      url: "/favicon.ico",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/assets/images/favicon/favicon-16x16.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/assets/images/favicon/favicon-32x32.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/assets/images/favicon/apple-touch-icon.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/assets/images/favicon/android-chrome-192x192.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/assets/images/favicon/android-chrome-512x512.png",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      url: "/favicon-dark.ico",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      url: "/assets/images/favicon/favicon-dark-16x16.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      url: "/assets/images/favicon/favicon-dark-32x32.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      url: "/assets/images/favicon/apple-touch-icon-dark.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      url: "/assets/images/favicon/android-chrome-dark-192x192.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      url: "/assets/images/favicon/android-chrome-dark-512x512.png",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
+};
